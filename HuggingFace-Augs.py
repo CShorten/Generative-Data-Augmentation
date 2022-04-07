3d7acecd743dbd55a5b976dba2f048ad55d70bda
Connor Shorten <connorshorten300@gmail.com>
2022-04-07 16:11:11 -0400

    Update HuggingFace-Augs.py

--- HuggingFace-Augs.py
+++ HuggingFace-Augs.py
@@ -1,1 +1,11 @@
+# download the model outside of the sampling function
+def get_model():
+  import transformers
+  model = transformers.AutoModelForCausalLM.from_pretrained("distilgpt2")
+  
 
+def GPT2_sampler():
+  from transformers import pipeline
+  pipe = pipeline("text-generation")
+  return pipe
+  
